8a5db18b9a33854da9101788a1c05f297716dae2
ambrizzzdev <5aariker@gmail.com>
2025-10-02 18:48:04 -0600

    a

--- home/index.js
+++ home/index.js
@@ -82,7 +82,7 @@ function updateProgress(){
   $("#err2").classList.remove("show");
 }
 
-const DEADLINE = new Date("2025-10-02T18:45:00"); // fecha y hora límite
+const DEADLINE = new Date("2025-10-02T18:50:00"); // fecha y hora límite
 
 function checkDeadline(){
   const now = new Date();
